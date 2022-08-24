57633f91c45e397f30aa632fea4a4f0a55835be1
Machoka Daisy <machokadaisy25@gmail.com>
2022-08-24 14:31:22 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(n1, n2)
+    n1 + n2
+end
+
+def halve(n)
+    if n.class != Integer
+        return nil
+    end
+
+    n / 2
+end
